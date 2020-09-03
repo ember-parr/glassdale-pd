d3afccfbb428f808eda407d5b752a248467e0d4c
Ember Parr <ember-devops@gmail.com>
2020-09-03 16:01:22 -0500

    case notes sidebar now includes dropdown to filter by suspect

--- scripts/notes/NoteList.js
+++ scripts/notes/NoteList.js
@@ -10,6 +10,7 @@ export const NotesList = () => {
   getNotes().then(() => {
     notesArray = useNotes();
     addNotesToDom(notesArray);
+    render(notesArray);
   });
 };
 
@@ -22,3 +23,52 @@ const addNotesToDom = (anArrayOfNotes) => {
 
   domElement.innerHTML = HTMLRender.join("");
 };
+
+//below this line i am trying to make a filter for suspects in the notes list
+
+const contentTarget = document.querySelector(".filters__notes");
+
+const render = (notesCollection) => {
+  const suspectNames = notesCollection.map((noteObj) => {
+    let suspectNameInArray = noteObj.suspect;
+    return suspectNameInArray;
+  });
+  const sortedArray = suspectNames.sort();
+  contentTarget.innerHTML = `
+            <select class="dropdown" id="asideForNotes">
+                <option value="0">Filter Notes by Suspect...</option>
+                ${sortedArray.map((suspectStr) => {
+                  return `<option>${suspectStr}</option>`;
+                })}
+            </select>
+        `;
+};
+
+const eventHub = document.querySelector("#asideForNotes");
+
+eventHub.addEventListener("change", (event) => {
+  if (event.target.id === "asideForNotes") {
+    const customEvent = new CustomEvent("suspectChosen", {
+      detail: {
+        suspectThatWasChosen: event.target.value,
+      },
+    });
+    console.log("suspect chosen: ", customEvent.detail.suspectThatWasChosen);
+    eventHub.dispatchEvent(customEvent);
+  }
+});
+
+let suspectThatWasSelected;
+eventHub.addEventListener("suspectChosen", (event) => {
+  suspectThatWasSelected = event.detail.suspectThatWasChosen;
+  if (event.detail.suspectThatWasChosen !== "0") {
+    const matchingSuspect = notesArray.filter((currentNote) => {
+      return currentNote.suspect === event.detail.suspectThatWasChosen;
+    });
+    addNotesToDom(matchingSuspect);
+  } else if (event.detail.suspectThatWasChosen === "0") {
+    addNotesToDom(notesArray);
+  } else {
+    console.log("error with eventHub line 62 and beyond");
+  }
+});
