afb4a78e112c81302b7fd58aa94e6f2bfa3c14eb
Ember Parr <ember-devops@gmail.com>
2020-09-03 22:18:04 -0500

    alibi now shows when button is pressed for each criminal

--- scripts/criminals/Criminals.js
+++ scripts/criminals/Criminals.js
@@ -1,5 +1,5 @@
 export const CriminalHTML = (criminalObj) => {
-  return `
+  const placeCriminalOnDom = `
     <section id="criminal-${criminalObj.id}" class="card-criminal">
         <h3>${criminalObj.name}</h3>
         <p>Age: ${criminalObj.age}</p>
@@ -11,7 +11,39 @@ export const CriminalHTML = (criminalObj) => {
           criminalObj.incarceration.end
         ).toLocaleDateString("en-US")}</p>
         <p>Arresting Officer: ${criminalObj.arrestingOfficer}</p>
-        <button id="associates--${criminalObj.id}">Associate Alibis</button>
-    </section>
-    `;
+        <button class="knownAssociatesBtn" id="associates--${
+          criminalObj.id
+        }">Associate Alibis</button>
+        <section id="knownAssociates--${criminalObj.id}">
+        </section>
+        </section>
+        `;
+
+  return placeCriminalOnDom;
+};
+
+// const eventHub = document.querySelector(".criminalsContainer");
+// let knownAssociates = [];
+
+// eventHub.addEventListener("click", (event) => {
+//   if (event.target.id.startsWith("associates--")) {
+//     const [prefix, criminalName] = event.target.id.split("--");
+//     const customevent = new CustomEvent("alibiClicked", {
+//       detail: {
+//         nameOfCriminalClicked: criminalName,
+//       },
+//     });
+//     console.log("name of criminal clicked: ", criminalName);
+//     eventHub.dispatchEvent(customevent);
+//   }
+// });
+
+export const CriminalAlibiHTML = (associateObj) => {
+  const placeCriminalOnDom = `
+          <h5>Known Associate: ${associateObj.name}</h5>
+          <p>Alibi Provided: ${associateObj.alibi}</p>
+          <hr>
+        `;
+
+  return placeCriminalOnDom;
 };
