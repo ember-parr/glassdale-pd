28a4aee275333543a8eff2d5d6e91b79e77cf5d1
Ember Parr <ember-devops@gmail.com>
2020-09-18 13:12:45 -0500

    added function in note provider to delete notes

--- scripts/criminals/CriminalProvider.js
+++ scripts/criminals/CriminalProvider.js
@@ -11,3 +11,11 @@ export const getCriminals = () => {
       criminals = parsedCriminals;
     });
 };
+
+
+const deleteNote = (noteId) => {
+  return fetch(`http://localhost:8088/notes/${noteId}`, {
+      method: "DELETE"
+  })
+      .then(getNotes)
+}
