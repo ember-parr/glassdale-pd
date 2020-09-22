f5a11de2c079d67e47fc3a1bbebc31433572f553
Ember Parr <ember-devops@gmail.com>
2020-09-22 15:18:52 -0500

    added facility provider

--- scripts/criminals/CriminalList.js
+++ scripts/criminals/CriminalList.js
@@ -1,24 +1,49 @@
 import { CriminalHTML, CriminalAlibiHTML } from "./Criminals.js";
 import { useCriminals, getCriminals } from "./CriminalProvider.js";
+import { getFacilities, useFacilities } from "../facility/FacilityProvider.js";
+import { getCriminalFacilities, useCriminalFacilities } from "../facility/CriminalFacilityProvider.js"
 
-let criminalArray = [];
+let criminals = [];
+let facilities;
+let crimFac;
 
 export const CriminalList = () => {
-  getCriminals().then(() => {
-    criminalArray = useCriminals();
-    // console.log("criminal array: ", criminalArray);
-    addCriminalToDom(criminalArray);
-  });
+  // Kick off the fetching of both collections of data
+  
+  getFacilities()
+  .then(getCriminals())
+  .then(getCriminalFacilities)
+  .then(
+      () => {
+          // Pull in the data now that it has been fetched
+          facilities = useFacilities()
+          crimFac = useCriminalFacilities()
+          criminals = useCriminals()
+
+          // Pass all three collections of data to render()
+          render(criminals, facilities, crimFac)
+      }
+  )
+  console.log("criminals: ", criminals)
 };
 
+const contentTarget = document.querySelector(".criminalsContainer");
+
 //add all the criminals to the dom
-const addCriminalToDom = (aCriminalArray) => {
-  const domElemenet = document.querySelector(".criminalsContainer");
-  let HTMLArray = aCriminalArray.map((singleCriminal) => {
-    return CriminalHTML(singleCriminal);
-  });
-  domElemenet.innerHTML = HTMLArray.join("");
-};
+const render = (criminalsToRender, allFacilities, allRelationships) => {
+  contentTarget.innerHTML = criminalsToRender.map(
+      (criminalObject) => {
+          const facilityRelationshipsForThisCriminal = allRelationships.filter(cf => cf.criminalId === criminalObject.id)
+          const facilities = facilityRelationshipsForThisCriminal.map(cf => {
+              const matchingFacilityObject = allFacilities.find(facility => facility.id === cf.facilityId)
+              return matchingFacilityObject
+          })
+          return CriminalHTML(criminalObject, facilities)
+      }
+  ).join("")
+}
+
+
 
 //start of event hubs
 const eventHub = document.querySelector(".container");
@@ -27,25 +52,24 @@ let crimeThatWasSelected;
 eventHub.addEventListener("crimeChosen", (event) => {
   crimeThatWasSelected = event.detail.crimeThatWasChosen;
   if (event.detail.crimeThatWasChosen !== "0") {
-    const matchingCriminals = criminalArray.filter((currentCriminal) => {
+    const matchingCriminals = criminals.filter((currentCriminal) => {
       return currentCriminal.conviction === event.detail.crimeThatWasChosen;
     });
     console.log("Matching Criminals: ", matchingCriminals);
-    addCriminalToDom(matchingCriminals);
+    render(matchingCriminals, facilities, crimFac);
     document.querySelector(".filters__officer").value = "0";
   }
 });
 
 eventHub.addEventListener("officerChosen", (event) => {
   if (event.detail.officerThatWasChosen !== "0") {
-    const matchingCriminals = criminalArray.filter((currentCriminal) => {
+    const matchingCriminals = criminals.filter((currentCriminal) => {
       return (
         currentCriminal.arrestingOfficer ===
-          event.detail.officerThatWasChosen &&
-        currentCriminal.conviction === crimeThatWasSelected
+          event.detail.officerThatWasChosen
       );
     });
-    addCriminalToDom(matchingCriminals);
+    render(matchingCriminals, facilities, crimFac);
   }
 });
 
@@ -65,7 +89,7 @@ const addAlibiToDom = (knownAssociates, id) => {
 eventHub.addEventListener("click", (event) => {
   if (event.target.id.startsWith("associates--")) {
     const [prefix, criminalId] = event.target.id.split("--");
-    let found = criminalArray.find((criminal) => criminal.id == criminalId);
+    let found = criminals.find((criminal) => criminal.id == criminalId);
     let foundAssociates = found.known_associates;
     let idToLocate = found.id;
     console.table(found.known_associates);
